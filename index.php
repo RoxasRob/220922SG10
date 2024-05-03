30388a558bf9b68dd2f4bc4e4b256ef9ea44ba7c
RoxasRob <rcruzso01@ucvvirtual.edu.pe>
2024-05-02 23:18:48 -0500

    feat: Login/Index mejorado

--- index.php
+++ index.php
@@ -6,29 +6,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="CSS/Styte.css">
-    <title>Inicio de Sesion</title>
+    <link rel="stylesheet" href="Styte.css">
+    <title>Inicio de Sesión</title>
 </head>
 <body>
-    <form action="IniSesion.php" method="POST">
-        <div class="my-form__wrapper"> 
-        <h1>INICIO DE SESION </h1>
-        <hr>
-        <i  class="fa-solid fa-user"></i>
-        <label>Usuario</label>
-        <input type="text" name="Usuario" placeholder="Nombre de Usuario">
-
-        <i class ="fa-solid fa-unlock"></i>
-        <label>Clave</label>
-        <input type="text" name = "Clave" placeholder="Clave" >
-        <hr>
-        <button type= "submit"> Inicio Sesion</button>
-        <a href="CrearCue.php">Crea Tu Cuenta</a>
+    <div class="logo-container">
+        <img src="ruta/a/tu/logo.png" alt="Logo" class="logo">
+    </div>
+    <form action="IniSesion.php" method="POST" class="my-form__wrapper">
+        <h1>BIENVENIDOS</h1>
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Usuario</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Contraseña</label>
+        </div>
+        <div>
+            <a href="#">Olvidé mi Clave</a>
+            <a href="#">Cambiar mi Clave</a>
+            <button type="submit">INICIO SESIÓN</button>
+            <span>¿No tienes una cuenta?</span>
+            <a href="CrearCue.php">Regístrate</a>
         </div>
     </form>
-
-
-    
-    
 </body>
 </html>
